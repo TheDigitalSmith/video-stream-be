48b7db3a9ae574fff7ac0f2a157c328f5a937390
infinityshades <tash.aneiq@gmail.com>
2020-04-13 16:26:57 +0200

    Refactored CRUDs, logger and init config

--- index.js
+++ index.js
@@ -1,65 +1,23 @@
+const debug = require('debug')('app:startup');
 const express = require ('express');
-const Joi = require('joi');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const {auth,log} = require ('./middleware/logger');
+const morgan = require('morgan');
+const genreService = require('./src/routes/genres');
+
 const app = express();
+dotenv.config();
 
 app.use(express.json());
+app.use(log);
+app.use(auth);
+app.use(helmet());
+app.use('/api/films/genre', genreService);
 
-const genres = [
-    {id: 1, genre:"thriller"},
-    {id: 2, genre:"action"},
-    {id: 3, genre:"comedy"},
-    {id: 4, genre:"horror"}
-]
-
-app.get('api/films/genre',(req,res)=>{
-    res.send(genres);
-})
-
-app.get('/api/films/:id', (req,res)=>{ 
-    const filmsGenre = genres.find(f => f.id === req.params.id);
-    if (!filmsGenre) return res.status(404).send('Genre not found');
-    
-    res.send(filmsGenre);
-})
-
-app.post('/api/films/genre', (req,res)=>{
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const newGenre = {
-        id: genres.length + 1,
-        genre: req.body.genre
-    }
-
-    genres.push(newGenre)
-    res.send(newGenre);
-})
-
-app.put('/api/films/:id', (req,res)=>{
-    const genreEdit = genres.find(g  => g.id === req.params.id);
-    if (!genreEdit) return res.status(404).send('Genre not found');
-    
-    const {error} = validateGenre(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    genreEdit.genre = req.body.genre;
-    res.send(genreToEdit);
-})
-
-app.delete('/api/films/:id', (req,res)=>{
-    const genreDelete = genres.find(g => g.id === req.params.id);
-    if (!genreDelete) return res.status(404).send('Genre not found');
-
-    const index = genres.indexOf(genreDelete);
-    genres.splice(index,1);
-    res.send(genreDelete);
-})
-
-function validateGenre(genre){
-    const schema = {
-        genre: Joi.min(3).required()
-    }
-    return Joi.validate(genre, schema);
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('tiny'));
+    debug('Morgan enabled...');
 }
 
 const port = process.env.PORT || 4110
